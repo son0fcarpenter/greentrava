8fc28f224d00c3e933e1fb29ab2be2bf1fe52883
AlexBlo <49761416+son0fcarpenter@users.noreply.github.com>
2023-08-03 09:55:03 +0300

    Update greentrava.js

--- js/greentrava.js
+++ js/greentrava.js
@@ -58,7 +58,20 @@ myMap.geoObjects.add(myPlacemark);
 
 
 function serializeForm(formNode) {
-  return new FormData(formNode)
+  const { elements } = formNode
+
+  const data = new FormData()
+
+  Array.from(elements)
+    .filter((item) => !!item.name)
+    .forEach((element) => {
+      const { name, type } = element
+      const value = type === 'checkbox' ? element.checked : element.value
+
+      data.append(name, value)
+    })
+
+  return data
 }
 
 
