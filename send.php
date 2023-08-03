6d6dd5826067e71fa962fc8a55dd1a677f28a4f6
AlexBlo <49761416+son0fcarpenter@users.noreply.github.com>
2023-08-03 11:57:53 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,1 +1,15 @@
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+
+$name = urldecode($name);
+$phone = urldecode($phone);
+
+$name = trim($name);
+$phone = trim($phone);
+
+echo $fio;
+echo "<br>";
+echo $email;
