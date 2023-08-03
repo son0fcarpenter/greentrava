168738ce7cd63078ccd35c932f1798bfe4846f8e
AlexBlo <49761416+son0fcarpenter@users.noreply.github.com>
2023-08-03 13:03:52 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,15 +1,39 @@
-$name = $_POST['name'];
+<html>
+<head>
+<title>Форма заявки с сайта</title>
+</head>
+<body>
+<?php
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['name']) and !isset($_POST['email'])){
+ ?><form id="order" class="feedback__form form flex" action="send.php" method="POST">
+    <div class="form__inner flex">
+    <input class="form__input form__phone" name="name" type="text" placeholder="Введите Ваше имя">
+    <input class="form__input form__email" name="phone" type="tel" placeholder="+7(999)999-99-99" required>              
+    <label class="form__check">
+      <input type="checkbox" required> <span class="form__textprivate">Нажимая на&nbsp;кнопку, я&nbsp;соглашаюсь с </span><a href="#">политикой конфиденциальности.</a>
+    </label>
+    </div>
+     <div class="form__blockbutton flex">
+      <button type="submit" class="form__send btn-reset">Отправить</button>
+      </div>            
+      </form><?php
+} else {
+ //показываем форму
+ $name = $_POST['name'];
 $phone = $_POST['phone'];
-
 $name = htmlspecialchars($name);
 $phone = htmlspecialchars($phone);
-
 $name = urldecode($name);
 $phone = urldecode($phone);
-
 $name = trim($name);
 $phone = trim($phone);
-
-echo $fio;
-echo "<br>";
-echo $email;
+ if (mail("plotnikasin@gmail.com", "Заявка с сайта", "Имя:".$name.". Телефон: ".$phone ,"From: alexdulepov@rambler.ru \r\n")){
+ echo "Сообщение успешно отправлено";
+ } else {
+ echo "При отправке сообщения возникли ошибки";
+ }
+}
+?>
+</body>
+</html>
