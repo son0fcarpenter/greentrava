2df3e5c96abd7b8176a15846e3c0c9d785525b98
AlexBlo <49761416+son0fcarpenter@users.noreply.github.com>
2023-08-06 10:29:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,24 +9,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let error = formValidate(form);
 
-    let formData = new formData(form);
+    // let formData = new formData(form);
 
     if (error === 0) {
-      form.classList.add('_sending');
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData
-      });
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        formPreview.innerHTML = '';
-        form.reset();
-        form.classList.remove('_sending');
-      } else {
-        alert("Ошибка");
-        form.classList.remove('_sending');
-        }
+      // form.classList.add('_sending');
+      // let response = await fetch('sendmail.php', {
+      //   method: 'POST',
+      //   body: formData
+      // });
+      // if (response.ok) {
+      //   let result = await response.json();
+      //   alert(result.message);
+      //   formPreview.innerHTML = '';
+      //   form.reset();
+      //   form.classList.remove('_sending');
+      // } else {
+      //   alert("Ошибка");
+      //   form.classList.remove('_sending');
+      //   }
       }  
     }
   }
